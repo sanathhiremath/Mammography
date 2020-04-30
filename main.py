cac50923503f38ad7ba1fc7bf437ec2e2dd68bb7
Sanath R Hiremath <sanath.hiremath@gmail.com>
2020-04-30 07:31:32 +0530

    model

--- main.py
+++ main.py
@@ -19,6 +19,38 @@ def index():
     return render_template("homepage.html")
 
 
+# -----------------Admin----------------------------------
+
+@app.route('/admin/passwordreset', methods=['GET', 'POST'])
+def resetpassword():
+    id = request.args['id']
+    user_type = request.args['user_type']
+    return render_template("doctorpasswordresetpage.html")
+
+
+@app.route('/admin/adminLogin', methods=['GET', 'POST'])
+def adminLogin():
+    if request.method == 'POST':
+        adusername = request.form["username"]
+        adpassword = request.form["password"]
+
+        adminuser = db.session.query(AdminModel).filter_by(email=adusername, password=adpassword).first()
+
+        db.session.commit()
+
+        if adminuser is None:
+            return render_template("adminregistration.html")
+        else:
+            win32api.MessageBox(0, adminuser.name + " LoggedIn", 'Admin Response')
+            return render_template("adminhomepage.html")
+    else:
+        return render_template("adminlogin.html")
+
+
+@app.route('/admin/adminLogin/adminhomepage', methods=['GET', 'POST'])
+def adminhomepage():
+    return render_template("adminhomepage.html")
+
 
 @app.route('/doctor', methods=['GET', 'POST'])
 def doctor():
@@ -39,11 +71,6 @@ def doctor():
         return render_template("doctorhomepage.html")
 
 
-@app.route('/doctor/passwordreset', methods=['GET', 'POST'])
-def doctorpaswordreset():
-    return render_template("doctorpasswordresetpage.html")
-
-
 @app.route('/doctor/doctorregistration', methods=['GET', 'POST'])
 def doctorregistration():
     if request.method == 'POST':
@@ -51,14 +78,15 @@ def doctorregistration():
         data = json.loads(y)
 
         # data = request.get_json()
-        new_doctor = DoctorModel(Name=data['name'], DOB=data['dob'], PhoneNumber=data['PhoneNumber'],
+        new_doctor = DoctorModel(name=data['name'], DOB=data['dob'], phone_number=data['PhoneNumber'],
                                  specialization=data['specialization'], email=data['email'], password=data['password'])
 
         db.session.add(new_doctor)
         db.session.commit()
+        win32api.MessageBox(0, str(new_doctor.id) + " LoggedIn", 'Admin Response')
 
-        email_body = "You are successfully registered {} <{}>.".format(new_doctor.Name, new_doctor.email)
-        email_html = render_template("doctorresetpassword.html", username=new_doctor.Name,
+        email_body = "You are successfully registered {} <{}>.".format(new_doctor.name, new_doctor.email)
+        email_html = render_template("doctorresetpassword.html", username=new_doctor.name,
                                      link="http://127.0.0.1:5000/doctor/passwordreset")
         send_email(new_doctor.email, '', "Confirmation Email", email_body, email_html)
         return render_template("adminhomepage.html")
@@ -67,32 +95,34 @@ def doctorregistration():
 
 
 @app.route('/api/GetDoctorDetails/<email>/<password>', methods=['GET', 'POST'])
-def GetDoctorDetails(email,password):
-    #return "My emil is " +email+ " and password is "+ password
-    #return "Hello"
-    #json = request.get_json()
+def GetDoctorDetails(email, password):
+    # return "My emil is " +email+ " and password is "+ password
+    # return "Hello"
+    # json = request.get_json()
     value = db.session.query(DoctorModel).filter_by(email=email, password=password).first()
-    #win32api.MessageBox(0, value.Name+value.email+str(value.DOB)+str(value.id)+value.PhoneNumber+value.specialization+value.password + " LoggedIn", 'Admin Response')
+    # win32api.MessageBox(0, value.Name+value.email+str(value.DOB)+str(value.id)+value.PhoneNumber+value.specialization+value.password + " LoggedIn", 'Admin Response')
 
     db.session.commit()
 
     if value is None:
-        r={"id":0}
-        res={"result":r}
+        r = {"id": 0}
+        res = {"result": r}
         result = json.dumps(res)
-        #win32api.MessageBox(0,"invalid",'Admin response')
+        # win32api.MessageBox(0,"invalid",'Admin response')
         return result
     else:
-        v={"id":int(value.id), "name":value.Name, "dob":str(value.DOB), "phonenumber":value.PhoneNumber,"specialization":value.specialization, "email":value.email, "password": value.password}
-        res={"result":v}
-        #win32api.MessageBox(0,str(v),'Admin Response')
+        v = {"id": int(value.id), "name": value.Name, "dob": str(value.DOB), "phonenumber": value.PhoneNumber,
+             "specialization": value.specialization, "email": value.email, "password": value.password}
+        res = {"result": v}
+        # win32api.MessageBox(0,str(v),'Admin Response')
 
-        result=json.dumps(res)
-        #result=Response(js, status=200, mimetype=)
-        #win32api.MessageBox(0, value.Name + " LoggedIn", 'Admin Response')
+        result = json.dumps(res)
+        # result=Response(js, status=200, mimetype=)
+        # win32api.MessageBox(0, value.Name + " LoggedIn", 'Admin Response')
         return result
 
 
+# -------------------------------radiologist-----------------------------------
 
 @app.route('/radiologist', methods=['GET', 'POST'])
 def radiologist():
@@ -121,7 +151,7 @@ def radiologistregistration():
         r = json.loads(re)
 
         # data = request.get_json()
-        new_radiologist = RadiologistModel(Name=r['name'], DOB=r['dob'], PhoneNumber=r['PhoneNumber'],
+        new_radiologist = RadiologistModel(name=r['name'], DOB=r['dob'], phone_number=r['PhoneNumber'],
                                            specialization=r['specialization'], email=r['email'], password=r['password'])
         db.session.add(new_radiologist)
         db.session.commit()
@@ -134,30 +164,7 @@ def radiologistregistration():
 # def admin():
 # return render_template("adminlogin.html")
 
-
-@app.route('/admin/adminLogin', methods=['GET', 'POST'])
-def adminLogin():
-    if request.method == 'POST':
-        adusername = request.form["username"]
-        adpassword = request.form["password"]
-
-        adminuser = db.session.query(AdminModel).filter_by(email=adusername, password=adpassword).first()
-
-        db.session.commit()
-
-        if adminuser is None:
-            return render_template("adminregistration.html")
-        else:
-            win32api.MessageBox(0, adminuser.Name + " LoggedIn", 'Admin Response')
-            return render_template("adminhomepage.html")
-    else:
-        return render_template("adminlogin.html")
-
-
-@app.route('/admin/adminLogin/adminhomepage', methods=['GET', 'POST'])
-def adminhomepage():
-    return render_template("adminhomepage.html")
-
+# ------------------patient-----------------------
 
 @app.route('/patient/patientregistration', methods=['GET', 'POST'])
 def patientregistration():
@@ -167,8 +174,8 @@ def patientregistration():
         p = json.loads(pa)
 
         # data = request.get_json()
-        new_patient = PatientModel(Name=p['name'], DOB=p['dob'], Guardianname=p['guardianname'],
-                                   PhoneNumber=p['PhoneNumber'], Address=p['address'], email=p['email'],
+        new_patient = PatientModel(name=p['name'], DOB=p['dob'], guardian_name=['guardianname'],
+                                   phone_number=p['PhoneNumber'], address=p['address'], email=p['email'],
                                    password=p['password'])
         db.session.add(new_patient)
         db.session.commit()
@@ -197,7 +204,5 @@ def patient():
         return render_template("patienthomepage.html")
 
 
-
 if __name__ == "__main__":
     app.run(debug=True)
-
